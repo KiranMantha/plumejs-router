f88c7e9bf1d8bb0fb58221ffa4162f729bda7c5e
KiranMantha <kiranv.mantha@gmail.com>
2023-03-26 10:53:47 +0530

    Fixed issue where route state is not getting preserved

--- dist/src/internalRouter.service.d.ts
+++ dist/src/internalRouter.service.d.ts
@@ -3,6 +3,7 @@ export declare class InternalRouter {
     private _currentRoute;
     private _template;
     private _unSubscribeHashEvent;
+    private _routeStateMap;
     startHashChange(): void;
     stopHashChange(): void;
     getTemplate(): {
