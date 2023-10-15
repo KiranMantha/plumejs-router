95db9526cdce3cc6fdef98d9946b02026c9cead3
KiranMantha <kiranv.mantha@gmail.com>
2023-10-15 14:07:44 +0400

    updated matchpath regex

--- dist/src/utils.js
+++ dist/src/utils.js
@@ -1,6 +1,6 @@
 const matchPath = (route, path) => {
     if (route && path) {
-        const pattern = new RegExp(route.replace(/:[^\s/]+/g, '([\\w-]+)'));
+        const pattern = new RegExp(route.replace(/:[^\s/]+/g, '(.+)'));
         return pattern.test(path);
     }
     return false;
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 const matchPath = (route: string, path: string): boolean => {
   if (route && path) {
-    const pattern = new RegExp(route.replace(/:[^\s/]+/g, '([\\w-]+)'));
+    const pattern = new RegExp(route.replace(/:[^\s/]+/g, '(.+)'));
     return pattern.test(path);
   }
   return false;
